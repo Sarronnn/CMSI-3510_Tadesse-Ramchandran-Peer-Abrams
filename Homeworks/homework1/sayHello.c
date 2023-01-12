bd8a547639717aa4eb832edbdb0a6754d008859f
Sarronnn <89826597+Sarronnn@users.noreply.github.com>
2023-01-11 17:26:22 -0800

    Create sayHello.c
    Compiled and run our sayHello program

---
+++ Homeworks/homework1/sayHello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main( int argc, char * argv[] ) {
+    char input [55];
+    printf("what is your name?");
+    gets(input); 
+    printf("Hello,%s\n", input);
+      
+
+}
