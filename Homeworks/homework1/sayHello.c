d3efb5cbb6e2f3874fc0f8346674b12a0b88a144
Sarronnn <89826597+Sarronnn@users.noreply.github.com>
2023-01-18 16:37:22 -0800

    Delete Homeworks/homework1 directory

--- Homeworks/homework1/sayHello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main( int argc, char * argv[] ) {
-    char input [55];
-    printf("what is your name?");
-    gets(input); 
-    printf("Hello,%s\n", input);
-}
