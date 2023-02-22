a100317703d0b3faead5f1952b0feef7bcb420dd
Sarronnn <89826597+Sarronnn@users.noreply.github.com>
2023-02-22 15:25:11 -0800

    Update BoundedBuffer2.java

--- Homeworks/homework02/BoundedBuffer2.java
+++ Homeworks/homework02/BoundedBuffer2.java
@@ -1,24 +1,25 @@
-public class BoundedBuffer2 {
+
+
+public class BoundedBuffer {
     private Object[] buffer = new Object[20]; // arbitrary size
     private int numOccupied = 0;
     private int firstOccupied = 0;
     
-    /* invariant: 0 <= numOccupied <= buffer.length
-    0 <= firstOccupied < buffer.length
-    buffer[(firstOccupied + i) % buffer.length]
-    contains the (i+1)th oldest entry,
-    for all i such that 0 <= i < numOccupied */
 
     public synchronized void insert(Object o)
     throws InterruptedException
     {
-        while(numOccupied == buffer.length)
+        while(numOccupied == buffer.length) {
         // wait for space
             wait();
+        }
         buffer[(firstOccupied + numOccupied) % buffer.length] = o;
         numOccupied++;
         // in case any retrieves are waiting for data, wake them
+        if(numOccupied == 1){
         notifyAll();
+        }
+
     }
     public synchronized Object retrieve()
     throws InterruptedException
@@ -31,7 +32,11 @@ public class BoundedBuffer2 {
         firstOccupied = (firstOccupied + 1) % buffer.length;
         numOccupied--;
         // in case any inserts are waiting for space, wake them
-        notifyAll();
+        if(numOccupied == buffer.length){
+            notifyAll();
+        }
         return retrieved;
     }
+
+   
 }
